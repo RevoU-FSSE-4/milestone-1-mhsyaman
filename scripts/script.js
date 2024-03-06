de8b5f42dbec7f3b604b1dce84a7efc5b3841692
Muhammad Syaman <mhmmdsyaman101@gmail.com>
2024-03-06 09:50:33 +0700

    adding function typing animation text in hero section

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const textToType = ["Hi, I'm", "Muhammad Syaman"];
+
+let index = 0;
+let wordIndex = 0;
+let speed = 100;
+
+const textToTypeElement = document.getElementById("typedText");
+
+function typeWriter() {
+    if (wordIndex < textToType[index].length) {
+        textToTypeElement.innerHTML += textToType[index].charAt(wordIndex);
+        wordIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        // Jika telah mengetik seluruh kata dalam elemen saat ini, lanjutkan ke elemen selanjutnya
+        textToTypeElement.innerHTML += "<br>"; // Tambahkan baris baru
+        index++; // Pindah ke elemen selanjutnya
+        wordIndex = 0; // Reset indeks kata
+        if (index < textToType.length) {
+            setTimeout(typeWriter, speed);
+        }
+    }
+}
+
+// Panggil fungsi untuk memulai mengetik
+typeWriter();
